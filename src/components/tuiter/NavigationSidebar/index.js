35e759924fb2bd874b3d84dbf3b2f7a7e3b45f48
Rosan <wang.ruox@northeastern.edu>
2022-03-20 17:54:33 -0400

    syntax changes part 2

--- src/components/tuiter/NavigationSidebar/index.js
+++ src/components/tuiter/NavigationSidebar/index.js
@@ -7,35 +7,35 @@ const NavigationSidebar = (
     return(
         <>
             <div className="list-group">
-                <i className="fab fa-twitter list-group-item"></i>
-                <i className="fas fa-home list-group-item d-xl-none"></i>
-                <i className="fas fa-hashtag list-group-item d-xl-none active"></i>
-                <i className="fas fa-bell list-group-item d-xl-none"></i>
-                <i className="fas fa-envelope list-group-item d-xl-none"></i>
-                <i className="fas fa-bookmark list-group-item d-xl-none"></i>
-                <i className="fas fa-list list-group-item d-xl-none"></i>
-                <i className="fas fa-user list-group-item d-xl-none"></i>
+                <i className="fab fa-twitter list-group-item"/>
+                <i className="fas fa-home list-group-item d-xl-none"/>
+                <i className="fas fa-hashtag list-group-item d-xl-none active"/>
+                <i className="fas fa-bell list-group-item d-xl-none"/>
+                <i className="fas fa-envelope list-group-item d-xl-none"/>
+                <i className="fas fa-bookmark list-group-item d-xl-none"/>
+                <i className="fas fa-list list-group-item d-xl-none"/>
+                <i className="fas fa-user list-group-item d-xl-none"/>
                 <div className="list-group-item d-xl-none ps-1">
                     <span className="fa-stack float-left">
-                        <i className="fas fa-circle fa-stack-1x"></i>
-                        <i className="fas fa-ellipsis-h fa-inverse fa-stack-1x"></i>
+                        <i className="fas fa-circle fa-stack-1x"/>
+                        <i className="fas fa-ellipsis-h fa-inverse fa-stack-1x"/>
                     </span>
                 </div>
 
-                <a href="#" className="list-group-item d-xl-block d-none"> <i className="fas fa-home"></i> Home </a>
+                <a href="#" className="list-group-item d-xl-block d-none"> <i className="fas fa-home"/> Home </a>
                 <a href="#" className="list-group-item active d-xl-block d-none"><i
-                    className="fas fa-hashtag"></i> Explore</a>
-                <a href="#" className="list-group-item d-xl-block d-none"><i className="fas fa-bell"></i> Notifications</a>
+    className="fas fa-hashtag"/> Explore</a>
+                <a href="#" className="list-group-item d-xl-block d-none"><i className="fas fa-bell"/> Notifications</a>
                 <a href="#" className="list-group-item d-xl-block d-none"><i
-                    className="fas fa-envelope"></i> Messages</a>
-                <a href="#" className="list-group-item d-xl-block d-none"><i className="fas fa-bookmark"></i> Bookmarks</a>
-                <a href="#" className="list-group-item d-xl-block d-none"><i className="fas fa-list"></i> Lists</a>
-                <a href="#" className="list-group-item d-xl-block d-none"><i className="fas fa-user"></i> Profile</a>
+    className="fas fa-envelope"/> Messages</a>
+                <a href="#" className="list-group-item d-xl-block d-none"><i className="fas fa-bookmark"/> Bookmarks</a>
+                <a href="#" className="list-group-item d-xl-block d-none"><i className="fas fa-list"/> Lists</a>
+                <a href="#" className="list-group-item d-xl-block d-none"><i className="fas fa-user"/> Profile</a>
 
                 <a href="#" className="list-group-item d-xl-block d-none">
                 <span className="fa-stack fa-sm  p-0">
-                    <i className="fas fa-circle fa-stack-2x"></i>
-                    <i className="fas fa-ellipsis-h fa-inverse fa-stack-1x"></i>
+                    <i className="fas fa-circle fa-stack-2x"/>
+                    <i className="fas fa-ellipsis-h fa-inverse fa-stack-1x"/>
                 </span> More
                 </a>
             </div>
